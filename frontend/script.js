e1f6d2868a278dd3101518aa4ef0d29ebab77312
ahmed Ali <abdinasirali587@gmail.com>
2025-03-15 10:20:25 +0100

    Add mobile responsive styles & images

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     // This function will execute when all HTML code is rendered
     // When DOM (Document Object Model) is loaded
+    const mobileMenu = document.getElementById("mobileMenu");
+    const navLinks = document.querySelector(".nav-links");
+    mobileMenu.addEventListener("click", function() {
+        navLinks.classList.toggle('active');
+    })
+
     
     let slideIndex = 0;
     const slides = document.querySelectorAll('.slide');
